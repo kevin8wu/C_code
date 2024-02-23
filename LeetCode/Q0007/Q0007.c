432f0d3da09c3897cf4f634f0095d48e80f5a6cc
kailin <kingsmanwu@icloud.com>
2024-02-23 21:12:19 +0800

    complete leetcode Q7

--- LeetCode/Q0007/Q0007.c
+++ LeetCode/Q0007/Q0007.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 int reverse(int x)
 {
+    int rev = 0;
+    int pop = 0;
+    
+    if(x == 0) return 0;
 
+    while(x != 0)
+    {
+        pop = x%10;
+        if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)){return 0;}
+        if(rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8)){return 0;}
+        rev = rev*10 + pop;
+
+        x = x/10;
+    }
+
+    return rev;
 }
 
 /*
@@ -30,7 +46,8 @@ public:
 
 int main()
 {
-
+    int x = 123;
+    printf("%d %d\n", x, reverse(x));
 
     return 0;
 }
